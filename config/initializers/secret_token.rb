115fcb131a99c05c9204b2fa0088d4844c66bc87
Alex Abreu <alexabreu@gmail.com>
2014-02-22 15:53:41 -0700

    Initial commit and project setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nethermead::Application.config.secret_key_base = '32852d02daf64c8de0b0264a57cf6904dadcbc12554a57f879b86951797ef27178f1b8a790cb4574e2631e5116dfb67ff5a1f96bee1846fd46596951253874c1'
