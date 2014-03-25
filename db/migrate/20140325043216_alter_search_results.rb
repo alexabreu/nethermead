c79a10cd56e4815324e2713a4aca17425ee034fc
nakpakpan-nethermead <nakpakpan@nethermead.com>
2014-03-25 00:21:47 -0500

    Fixed errors in search_results table (FD-16)
    fixed error in the search_results table described in JIRA ticket FD-16

---
+++ db/migrate/20140325043216_alter_search_results.rb
@@ -0,0 +1,27 @@
+class AlterSearchResults < ActiveRecord::Migration
+  def change
+	remove_column :search_results, :num_enrollee_in_state
+  	remove_column :search_results, :num_enrollee_in_state_w_company
+  	remove_column :search_results, :num_enrollee_in_county
+  	remove_column :search_results, :share_company_in_state
+  	remove_column :search_results, :num_company_in_state
+  	remove_column :search_results, :num_company_in_county
+  	remove_column :search_results, :num_company_plan_in_county 
+  	remove_column :search_results, :num_plantype_in_county
+    	remove_column :search_results, :county character varying
+
+	add_column :search_results, :state_enrolled, :bigint
+  	add_column :search_results, :state_parent_enrolled, :bigint
+  	add_column :search_results, :share_company_in_state, :float
+  	add_column :search_results, :num_active_competitors, :bigint
+  	add_column :search_results, :reportdate, :datetime
+  	add_column :search_results, :share_parent_in_state_rank, :bigint
+  	add_column :search_results, :available_market, :integer
+  	add_column :search_results, :total_market, :integer
+  	add_column :search_results, :competition_status, :text
+  	add_column :search_results, :share_parent_in_state_ss :float
+  	add_column :search_results, :num_similar_plans, :bigint
+  	add_column :search_results, :num_op_county, :bigint
+
+  end
+end
