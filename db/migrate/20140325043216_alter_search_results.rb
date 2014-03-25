6e6c476f0f11d8a606c305be4007ae9cdffbb059
Phillip Nelson <phillipnelson@gmail.com>
2014-03-24 23:30:16 -0700

    fix migration syntax. add bootstrap theme to application css.

--- db/migrate/20140325043216_alter_search_results.rb
+++ db/migrate/20140325043216_alter_search_results.rb
@@ -1,6 +1,6 @@
 class AlterSearchResults < ActiveRecord::Migration
   def change
-	remove_column :search_results, :num_enrollee_in_state
+	  remove_column :search_results, :num_enrollee_in_state
   	remove_column :search_results, :num_enrollee_in_state_w_company
   	remove_column :search_results, :num_enrollee_in_county
   	remove_column :search_results, :share_company_in_state
@@ -8,9 +8,9 @@ class AlterSearchResults < ActiveRecord::Migration
   	remove_column :search_results, :num_company_in_county
   	remove_column :search_results, :num_company_plan_in_county 
   	remove_column :search_results, :num_plantype_in_county
-    	remove_column :search_results, :county 
+    remove_column :search_results, :county
 
-	add_column :search_results, :state_enrolled, :bigint
+	  add_column :search_results, :state_enrolled, :bigint
   	add_column :search_results, :state_parent_enrolled, :bigint
   	add_column :search_results, :share_company_in_state, :float
   	add_column :search_results, :num_active_competitors, :bigint
@@ -19,9 +19,8 @@ class AlterSearchResults < ActiveRecord::Migration
   	add_column :search_results, :available_market, :integer
   	add_column :search_results, :total_market, :integer
   	add_column :search_results, :competition_status, :text
-  	add_column :search_results, :share_parent_in_state_ss :float
+  	add_column :search_results, :share_parent_in_state_ss, :float
   	add_column :search_results, :num_similar_plans, :bigint
   	add_column :search_results, :num_op_county, :bigint
-
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140323011111) do
+ActiveRecord::Schema.define(version: 20140325043216) do
 
   create_table "companies", force: true do |t|
     t.string   "name_upper"
@@ -41,21 +41,24 @@ ActiveRecord::Schema.define(version: 20140323011111) do
     t.string   "company_name"
     t.string   "state_name"
     t.string   "product_class"
-    t.integer  "num_enrollee_in_state"
-    t.integer  "num_enrollee_in_state_w_company"
-    t.integer  "num_enrollee_in_county"
-    t.decimal  "share_company_in_state"
-    t.integer  "num_company_in_state"
-    t.integer  "num_company_in_county"
-    t.integer  "num_company_plan_in_county"
-    t.integer  "num_plantype_in_county"
     t.integer  "reportyear"
-    t.string   "county"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "company_id"
     t.integer  "product_class_id"
     t.integer  "state_id"
+    t.integer  "state_enrolled"
+    t.integer  "state_parent_enrolled"
+    t.float    "share_company_in_state"
+    t.integer  "num_active_competitors"
+    t.datetime "reportdate"
+    t.integer  "share_parent_in_state_rank"
+    t.integer  "available_market"
+    t.integer  "total_market"
+    t.text     "competition_status"
+    t.float    "share_parent_in_state_ss"
+    t.integer  "num_similar_plans"
+    t.integer  "num_op_county"
   end
 
   add_index "search_results", ["company_id"], name: "index_search_results_on_company_id"
