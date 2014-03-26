ac7d2535db1569df57a50e17ddcb4b2ef806e127
Phillip Nelson <phillipnelson@gmail.com>
2014-03-26 15:16:27 -0700

    accessor for percentages

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -2,4 +2,11 @@ class SearchResult < ActiveRecord::Base
   belongs_to :company
   belongs_to :product_class
   belongs_to :state
+
+  def percentage_market_share
+    if share_company_in_state
+      return share_company_in_state * 100
+    end
+    return 0
+  end
 end
