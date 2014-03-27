1dfe850e4c95bf97b1ce59d7ae6b818ee4c78262
Phillip Nelson <phillipnelson@gmail.com>
2014-03-26 19:16:37 -0700

    county list

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -3,10 +3,18 @@ class SearchResult < ActiveRecord::Base
   belongs_to :product_class
   belongs_to :state
 
+  def counties
+    ['County A', 'County B', 'County C', 'County D', 'County E']
+  end
+
+  def state_competition
+    [['Company A', 55], ['County B', 10], ['County C', 10],['County D', 10], ['County E', 10], ['Other', 05]]
+  end
+
   def percentage_market_share
     if share_company_in_state
       return share_company_in_state * 100
     end
-    return 0
+    return 20
   end
 end
