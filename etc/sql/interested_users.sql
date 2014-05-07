9529b12b5a2112cccfd54fa5fc475ae38a793a4f
nakpakpan-nethermead <nakpakpan@nethermead.com>
2014-05-07 13:27:20 -0500

    added new script files

---
+++ etc/sql/interested_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO interested_users(id, email, created_at, updated_at) VALUES (1,E'alex@nethermead.com',null,null);
+INSERT INTO interested_users(id, email, created_at, updated_at) VALUES (2,E'phillipnelson+test2@gmail.com',null,null);
+INSERT INTO interested_users(id, email, created_at, updated_at) VALUES (3,E'test@testing.com',null,null);
+INSERT INTO interested_users(id, email, created_at, updated_at) VALUES (4,E'yes@no.com',null,null);
+INSERT INTO interested_users(id, email, created_at, updated_at) VALUES (5,E'great@fantastic.com',null,null);
---
+++ etc/sql/product_classes.sql
@@ -0,0 +1,7 @@
+INSERT INTO product_classes(id, "name", abbr, img_ref, created_at, updated_at) VALUES (1,E'Medicare',E'MCR',null,null,null);
+INSERT INTO product_classes(id, "name", abbr, img_ref, created_at, updated_at) VALUES (2,E'Commercial Individual',E'IND',null,null,null);
+INSERT INTO product_classes(id, "name", abbr, img_ref, created_at, updated_at) VALUES (3,E'Commercial Small Group',E'SMG',null,null,null);
+INSERT INTO product_classes(id, "name", abbr, img_ref, created_at, updated_at) VALUES (4,E'Exchange Individual',E'XIN',null,null,null);
+INSERT INTO product_classes(id, "name", abbr, img_ref, created_at, updated_at) VALUES (5,E'Exchange SHOP',E'XSH',null,null,null);
+INSERT INTO product_classes(id, "name", abbr, img_ref, created_at, updated_at) VALUES (6,E'Medicaid',E'MCD',null,null,null);
+INSERT INTO product_classes(id, "name", abbr, img_ref, created_at, updated_at) VALUES (7,E'Commercial Large Group',E'LGP',null,null,null);
