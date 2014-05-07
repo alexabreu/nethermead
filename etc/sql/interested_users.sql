f251b7f111a1af0a07a5d5e8d53d22d662df1125
nakpakpan-nethermead <nakpakpan@nethermead.com>
2014-05-07 11:09:49 -0500

    deleted load scripts files

--- etc/sql/interested_users.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO interested_users(id, email, created_at, updated_at) VALUES (1,E'alex@nethermead.com',2014-03-23 21:01:25.601320000,2014-03-23 21:01:25.601320000);
-INSERT INTO interested_users(id, email, created_at, updated_at) VALUES (2,E'phillipnelson+test2@gmail.com',2014-03-25 01:43:40.782897000,2014-03-25 01:43:40.782897000);
-INSERT INTO interested_users(id, email, created_at, updated_at) VALUES (3,E'test@testing.com',2014-03-25 18:42:57.331156000,2014-03-25 18:42:57.331156000);
-INSERT INTO interested_users(id, email, created_at, updated_at) VALUES (4,E'yes@no.com',2014-03-25 18:43:15.519062000,2014-03-25 18:43:15.519062000);
-INSERT INTO interested_users(id, email, created_at, updated_at) VALUES (5,E'great@fantastic.com',2014-03-25 18:43:38.042217000,2014-03-25 18:43:38.042217000);
--- etc/sql/product_classes.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO product_classes(id, "name", abbr, img_ref, created_at, updated_at) VALUES (1,E'Medicare',E'MCR',null,2014-03-24 01:40:54.297598000,2014-03-24 01:40:54.297598000);
-INSERT INTO product_classes(id, "name", abbr, img_ref, created_at, updated_at) VALUES (2,E'Commercial Individual',E'IND',null,2014-03-24 01:40:54.297598000,2014-03-24 01:40:54.297598000);
-INSERT INTO product_classes(id, "name", abbr, img_ref, created_at, updated_at) VALUES (3,E'Commercial Small Group',E'SMG',null,2014-03-24 01:40:54.297598000,2014-03-24 01:40:54.297598000);
-INSERT INTO product_classes(id, "name", abbr, img_ref, created_at, updated_at) VALUES (4,E'Exchange Individual',E'XIN',null,2014-03-24 01:40:54.297598000,2014-03-24 01:40:54.297598000);
-INSERT INTO product_classes(id, "name", abbr, img_ref, created_at, updated_at) VALUES (5,E'Exchange SHOP',E'XSH',null,2014-03-24 01:40:54.297598000,2014-03-24 01:40:54.297598000);
-INSERT INTO product_classes(id, "name", abbr, img_ref, created_at, updated_at) VALUES (6,E'Medicaid',E'MCD',null,2014-03-24 01:40:54.297598000,2014-03-24 01:40:54.297598000);
-INSERT INTO product_classes(id, "name", abbr, img_ref, created_at, updated_at) VALUES (7,E'Commercial Large Group',E'LGP',null,2014-03-24 01:40:54.297598000,2014-03-24 01:40:54.297598000);
